e54c5b951e33a3e2a5ff3072347248e61048943e
tyckobra <tycko.bergman@elev.ga.ntig.se>
2022-11-11 09:11:17 +0100

    🐛 22-11-11

--- src/_data/github.js
+++ src/_data/github.js
@@ -2,10 +2,10 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 require('dotenv').config();
 
 module.exports = async function() {
-    const githubUser = process.env.GITHUB_USER;
+   /* const githubUser = process.env.GITHUB_USER;
     if (!githubUser) {
         throw new Error('Please set a GITHUB_USER environment variable');
-    }
+    }*/
     let url = `https://api.github.com/users/${githubUser}`;
 
     /* This returns a promise */
